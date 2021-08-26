7ca23cf9800cb37860ec2b4866a93d373c494185
brendan-janssen-medwing <75126018+brendan-janssen-medwing@users.noreply.github.com>
2021-08-26 09:35:03 +0200

    Update wellbeing.js

--- wellbeing.js
+++ wellbeing.js
@@ -1,36 +1,136 @@
-function showModal(url) {
-  modal.style.display = "block";
-  modaliframe.src = url;
-}
 
-// Get the modal
-var modal = document.getElementById("myModal");
-var modaliframe = document.getElementById("modaliframe");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var g_modal;
+var g_modaliframe;
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  closeModal();
-}
+//This hack essentially allows us to catch the child details iframe has had an edit occur
+//and that we should refresh the parent so that the summary calculates again.
+//We use the global variable because we are allowed to know when an iframe has reloaded via the onload event
+//but not to which URL it loaded (security cross-domain blah blah)
+var g_triggerExpected = true; //first page load
 
-function closeModal(){
-  modal.style.display = "none";
-  reload_iframes();
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+function bootload(){
+  invokeIframes();
+  hideContentIfNoResults(`+ facilityReviewExists(facilityId) +`);
+  
+  var element = document.getElementById("urlId");
+  element.innerHTML = "`+ thisURL +`";
+
+  // Get the modal
+  g_modal = document.getElementById("myModal");
+  g_modaliframe = document.getElementById("modaliframe");
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == g_modal) {
+      closeModal();
+    }
+  };
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
     closeModal();
+  };
+}
+
+
+function showingNoContentScreen(){
+  var element = document.getElementById("noReviews");
+  if (element.style.display == "block")
+  {
+    return true;
+  }
+
+  return false;
+}
+
+function hideContentIfNoResults(showContent){
+  if (!showContent){
+    var element = document.getElementById("noReviews");
+    element.style.display = "block";
+  }
+  else 
+  {
+    var element = document.getElementById("content");
+    element.style.display = "block";
   }
 }
 
+function invokeIframes(){
+  iFrameResize({ log: true }, '#summaryIframe');
+  iFrameResize({ log: true }, '#detailsIframe');
+}
+
+
+function loadDetailsPane(view){
+
+  var detailsIframe = document.getElementById("detailsIframe");
+
+  switch(view) {
+  case 1:
+    // all non archived reviews
+    g_triggerExpected = true;
+    detailsIframe.src = "`+ detailsURL + `";
+    break;
+  case 2:
+    g_triggerExpected = true;
+    detailsIframe.src = "`+ detailsCandidateOnlyURL + `";
+    break;
+  case 3: 
+    g_triggerExpected = true;
+    detailsIframe.src = "`+ detailsArchived + `";
+    break;
+  default:
+    g_triggerExpected = true;
+    detailsIframe.src = detailsIframe.src;
+    break;
+  }
+}
+
+function onLoadHandler_ReloadIfEditingWasDoneInsideTheIFrame(){
+  if (g_triggerExpected)
+  {
+    g_triggerExpected = false;
+    return;
+  }
+
+  //we want to update both the summary and rehit the details
+  reload_iframes();
+}
+
+function showModal(url) {
+  g_modal.style.display = "block";
+  modaliframe.src = url;
+}
+
+function closeModal(){
+  g_modal.style.display = "none";
+  reload_iframes();
+}
+
+
+function reload_page(){
+   alert(window.location.href);
+   alert(document.URL);
+
+   window.location.replace("` + thisURL + `");
+}
+
 function reload_iframes() {
-    var f_list = document.getElementsByTagName('iframe');
- 
-    for (var i = 0, f; f = f_list[i]; i++) {
-        f.src = f.src;
+    
+    if (showingNoContentScreen()){
+      reload_page();
+      return;
+      //unfortunately, I can't get a reload page working so this is a workaround for now. Just to show all of the page.
     }
+    
+    g_triggerExpected=true;
+    loadDetailsPane();
+
+    var summaryIframe = document.getElementById("summaryIframe");
+    summaryIframe.src = summaryIframe.src;
 }
